71eb63b6f54a1cd7b70a9e7a51a90862691f9e72
HarshalPatil <patilharshal2282@gmail.com>
2026-06-29 15:21:15 +0530

    Add gitignore and setup React frontend

---
+++ Task-1/my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 Harshal Patil/>
+      <li
+      heloo
+      manaste  
+      />
+      
+    </div>
+  );
+}
+
+export default App;
